5f5dd83606af685217ba935de0b355173ab55530
ignacio <ignaciomarucco@gmail.com>
2022-04-27 20:08:12 -0300

    Add DOM query selectors

--- scripts.js
+++ scripts.js
@@ -1,12 +1,17 @@
-// Operations Functions
+//DOM Query Selectors
+const numBtns = document.querySelectorAll('div.numBtns button');
+const operatorBtns = document.querySelectorAll('div.operatorBtns button');
+const equalBtn = document.querySelector('#equal');
+const clearBtn = document.querySelector('#clear');
+let display = document.querySelector('#display');
+
+// Basic Math Operations Functions
 const add = (a,b) => a + b;
-
 const subtract = (a,b) => a - b;
-
 const multiply = (a,b) => a * b;
-
 const divide = (a,b) => a / b;
 
+//Operate Function
 let operate = (num1,num2,operator) => {
     if(operator == `addOperator`) {
         result = add(num1,num2);
@@ -17,6 +22,7 @@ let operate = (num1,num2,operator) => {
     } else if (operator == `divisionOperator`) {
         result = divide(num1,num2);
     }
+    // operatorPressed = false;
     console.log(num1);
     console.log(num2);
     console.log(operator);
