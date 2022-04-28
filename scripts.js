26e0a443e776917596da25512c70694707464734
ignacio <ignaciomarucco@gmail.com>
2022-04-28 00:19:15 -0300

    Rework several functions.

--- scripts.js
+++ scripts.js
@@ -1,87 +1,85 @@
 //DOM Query Selectors
-const numBtns = document.querySelectorAll('div.numBtns button');
-const operatorBtns = document.querySelectorAll('div.operatorBtns button');
-const equalBtn = document.querySelector('#equal');
-const clearBtn = document.querySelector('#clear');
-let display = document.querySelector('#display');
-
-// Basic Math Operations Functions
-const add = (a,b) => a + b;
-const subtract = (a,b) => a - b;
-const multiply = (a,b) => a * b;
-const divide = (a,b) => a / b;
-
-//Operate Function
-let operate = (num1,num2,operator) => {
-    if(operator == `addOperator`) {
-        result = add(num1,num2);
-    } else if (operator == `subtractOperator`) {
-        result = subtract(num1,num2);
-    } else if (operator == `multiplicationOperator`) {
-        result = multiply(num1,num2);
-    } else if (operator == `divisionOperator`) {
-        result = divide(num1,num2);
-    }
-    console.log(`num1:${num1},\noperator:${operator},\nnum2:${num2},\nresult:${result},\noperatorPressed:${operatorPressed}`);
-
-    displayFunction(result);
-}
-// Global Variables
-let operatorPressed = false;
-let num1 = 0;
-let num2 = 0;
-let operator;
-let result;
-
-
-//Store Num Function
-let storeNum = (e) => {
-    if (operatorPressed) {
-        num2 = Number(`${num2}${e.target.value}`);
-        displayFunction(num2);
-    } else {
-        num1 = Number(`${num1}${e.target.value}`);
-        displayFunction(num1);
-    }
-}
-
-//Display Function
-let displayFunction = (num) => {
-    display.textContent = num;
-}
-
-//Clear Display Function
-let clearDisplay = () => {
-    num1 = 0;
-    num2 = 0;
-    operatorPressed = false;
-    display.textContent = 0;
-}
-
-
-//Store Operator Function
-let storeOperator = (e) => {
-    if(operatorPressed === false) {
-        operatorStatus();
-    }
-    operator = `${e.target.id}`;
-}
-
-//Operator Status Changer Function
-let operatorStatus = () => {
-    if (operatorPressed === false) {
-        operatorPressed = true;
-    } else {
-        operatorPressed = false;
-    }
-}
+const operandBtns = document.querySelectorAll('.operand');
+const operatorBtns = document.querySelectorAll('.operator');
+const display = document.querySelector('.display');
+const equalBtn = document.querySelector('.equal');
+const clearBtn = document.querySelector('.clear');
 
 //Event Listeners
-numBtns.forEach(button => button.addEventListener('click', storeNum));
-operatorBtns.forEach(button => button.addEventListener('click', storeOperator));
-equalBtn.onclick = () => operate(num1,num2,operator);
-clearBtn.addEventListener('click', clearDisplay);
+operandBtns.forEach(operand => operand.addEventListener('click', storeOperands));
+operatorBtns.forEach(operator => operator.addEventListener('click', storeOperator));
+equalBtn.addEventListener('click',operate);
+clearBtn.addEventListener('click',clearAll);
 
+//Basic Math Operations Functions
+const add = (operand1,operator) => operand1 + operator;
+const subtract = (operand1,operator) => operand1 - operator;
+const multiply = (operand1,operator) => operand1 * operator;
+const divide = (operand1,operator) => {
+    if(operator == 0){
+        return `lmao`;
+    }
+    else{
+    return Math.floor(operand1 / operator);
+    }
+}
 
+//Global Variables
+let operation = ``;
+let operand = ``;
+let firstOperand = ``;
+let result = ``;
 
+//Operate Function
+function operate(operand1,operator,operand2){
+    if(operator == `+`){
+        result = add(Number(operand1),Number(operand2));
+        display.textContent = result;
+        operand = result;
+        operation = ``;
+        return result;
+    } else if(operator == `*`){
+        result = multiply(Number(operand1),Number(operand2));
+        display.textContent = result;
+        operand = result;
+        operation = ``;
+        return result;
+    } else if(operator == `-`){
+        result = subtract(Number(operand1),Number(operand2));
+        display.textContent = result;
+        operand = result;
+        operation = ``;
+        return result;
+    } else if(operator == `/`){
+        result = divide(Number(operand1),Number(operand2));
+        display.textContent = result;
+        operand = result;
+        operation = ``;
+        return result;
+        }
+    operate(firstOperand, operation, operand);
+}
 
+//Store Operands Function
+function storeOperands(e){
+    operand += e.target.textContent;
+    display.textContent = operand;
+}
+//Store Operator Function
+function storeOperator(e){
+    if(operation == `+` || operation == `-` || operation == `*` || operation == `/`){
+        operate(firstOperand, operation, operand);
+    }
+    firstOperand = operand;
+    operand = ``;
+    operation = e.target.textContent;
+    display.textContent = operation;
+}
+//Clear All Function
+function clearAll(){
+    display.textContent = `0`;
+    result = ``;
+    firstOperand = ``;
+    operand = ``;
+    operation = ``;
+}
